bafa6266c227721977cc3676b51e6b243f1fcbab
nytakoe1 <88328194+nytakoe1@users.noreply.github.com>
2022-03-06 12:47:58 +0300

    Update ex01.hpp

--- ex01.hpp
+++ ex01.hpp
@@ -1,7 +1,3 @@
-
-//change the below code
-// the function below now only works with integers, change it to work with any type
-// also fill out the body
 template < typename T >
 void multiples (T& sum, T x, int n)
 {
